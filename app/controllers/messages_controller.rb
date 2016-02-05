60a83ba8e17dd518297029122ff380ac4a7c276c
seiji_okura <slower.than.snails@gmail.com>
2016-02-05 15:34:09 +0000

    challenge1 release4

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,20 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
-    
-    #get all messages
     @messages = Message.all
   end
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path, notice:'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path, notice:'メッセージを保存しました'
+    else
+      #
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+    
   end
   
   private
