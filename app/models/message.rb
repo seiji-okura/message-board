8980ed1b939bffc1d6b1fd7998334419bcd74afe
seiji_okura <slower.than.snails@gmail.com>
2016-02-06 15:43:12 +0000

    challenge2 release1

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,15 @@ class Message < ActiveRecord::Base
   #内容は必須入力かつ2文字以上30文字以下
   validates :body, length:{minimum: 2, maximum: 30}, presence: true
   
+  validates :age, numericality:{only_integer: true}, presence: true
+  
+  validate :age_validation
+  
+  #18歳未満はNG
+  def age_validation
+    if age < 18
+      errors[:base] << "%{year}年ROMってろ！" %{year: (18 - age)}
+    end
+  end
+  
 end
